070a7a7f6c52d835167930e7cdfcd0fe3f348475
creatikme <40242736+creatikme@users.noreply.github.com>
2022-11-01 15:28:01 +0530

    1 binaries uploaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import Header from './components/Header';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-     
+      <Header />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
